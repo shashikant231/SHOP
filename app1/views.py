32c9d1e0a6e155edb849ad60442d5364461afacf
shashikant231 <shashikantching@gmail.com>
2022-06-23 14:13:54 +0530

    added search filter and stored secret key into environment variable

--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,4 @@
-import imp
+from rest_framework import filters
 from rest_framework import viewsets
 from .serializers import *
 from .models import *
@@ -8,7 +8,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter]
     search_fields = ["id","title"]
     filterset_fields = ["is_active", "price"]
     ordering_fields = ["amount", "price"]
@@ -17,10 +17,12 @@ class ProductViewSet(viewsets.ModelViewSet):
 class ShopViewSet(viewsets.ModelViewSet):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
+    filter_backends = [filters.SearchFilter]
     search_fields = ["title"]
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = [filters.SearchFilter]
     search_fields = ["id", "title"]
