182bbba3e8c9f470b782d0bfb34138b925a79ce3
shashikant231 <shashikantching@gmail.com>
2022-07-10 16:51:47 +0530

    changed model meta fields

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from unicodedata import category
 from django.db import models
 
@@ -6,6 +7,10 @@ class Shop(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to="uploads/")
+
+    class Meta:
+        verbose_name = "Shop"
+        verbose_name_plural = "Shops"
 
     def __str__(self):
         return self.title
